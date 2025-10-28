12d5fcf65a2eba23f633b94a5e05f60edcb43d39
vipashadhiman11 <vipashadhiman11@gmail.com>
2025-10-28 21:25:31 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -135,10 +135,9 @@ if st.button("Submit", type="primary"):
     
     collector = Agent(
         role = "Articles collector",
-        goal = "Asks the user about the {topic} and collects the articles releated to that topic using tool.",
+        goal = "Asks the user about the {topic} and collects the articles releated to that topic.",
         backstory = "The {topic} will be an organisation of stock name. Don't take any other input except topic"
                     "fetch the articles.\n",
-        tools=[get_articles_APItube],
         llm = llm,
         allow_delegation = False,
         verbose = False
